399f19071a063ec29d2cc8c7fab4fa81677be652
brokerji <470304574@qq.com>
2017-08-09 15:03:56 +0800

    Update impJsonToKib.py

--- impJsonToKib.py
+++ impJsonToKib.py
@@ -4,44 +4,32 @@ from datetime import datetime
 
 def timestamp_datetime(timestamp):
     dt=datetime.fromtimestamp(timestamp / 1000.0)
-    dt1=datetime.isoformat(dt)
-    return dt1
+    dt_format=datetime.isoformat(dt)
+    return dt_format
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
+    inputfile = sys.argv[1]
     outputfile = sys.argv[2]
-    with open(filename) as f:
-        pop_data = json.load(f)
+    fo = open (outputfile,'a')
+    with open(inputfile) as fi:
+        pop_data = json.load(fi)
         for i in range(0,len(pop_data['allItems'])):
-           # print(pop_data['allItems'][i])
-            s = pop_data['allItems'][i]
-            #print(type(s))
-            #   print("s")
-            f = open (outputfile,'a')
-            if ('highlight' in s.keys()) and ('start_time' in s.keys()) and ('end_time' in s.keys()):
-                #print(type(s['highlight']))
-                #print(s['highlight'])
+            s = pop_data['allItems'][i]          
+            if ('highlight' in s.keys()) and ('start_time' in s.keys()) and ('end_time' in s.keys()):         
                 del s['highlight']
                 print (type(s['start_time']))
                 s['start_time'] = timestamp_datetime(s['start_time'])
                 s['end_time'] = timestamp_datetime(s['end_time'])
-               # print(s)
                 jsObj=json.dumps(s)
-                f.write('{"index":{"_index":"report","_type":"data","_id":')
-                f.write(str(i))
-                f.write("}}")
-                f.write("\n")
-                f.write(jsObj)
-                f.write("\n")
+                fo.write('{"index":{"_index":"report","_type":"data","_id":')
+                fo.write(str(i))
+                fo.write("}}")
+                fo.write("\n")
+                fo.write(jsObj)
+                fo.write("\n")
             else :
                 jsObj=json.dumps(s)
-                #f.write('{"index":{"_index":"report","_type":"data","_id":')
-                #f.write(str(i))
-                #f.write("}}")
-                #f.write("\n")
-                #f.write(jsObj)
-                #f.write("\n")
-        f.close()
+        fo.close()
 
 
 
